9f1c8d8584b63a032aad67ff74ca31bc471025cd
Shipra Aggarwal <34913416+AggarwalShipra@users.noreply.github.com>
2018-03-30 22:18:24 +0530

    Add files via upload

---
+++ safe_state_que_usingthreads.c
@@ -0,0 +1,133 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<semaphore.h>
+#include<pthread.h>
+//fxn to display all datastructures values.
+int no_of_pro,no_of_res;
+int inst[5],avail_inst[3];
+int alloc[5][3],max[5][3],need[5][3],work[3],finish[5];
+sem_t a;
+void *display()
+{
+	int i,j;
+	printf("Number of process are %d \n",no_of_pro);
+	printf("Number of resources are %d \n",no_of_res);
+	printf("\nTotal Instances\n");
+	for(i=0;i<no_of_res;i++)
+	{
+		printf("%c --> %d ",i+65,inst[i]);
+	}
+	printf("\nAvailable Instances\n");
+	for(i=0;i<no_of_res;i++)
+	{
+		printf("%c --> %d ",i+65,avail_inst[i]);
+	}
+	printf("\nProcess\tAllocated Resource");
+	for(i=0;i<no_of_pro;i++)
+	{	
+		printf("\n%d\t",i);
+		for(j=0;j<no_of_res;j++)
+		{
+			printf("%c->%d\t",j+65,alloc[i][j]);		
+		}
+	}
+	printf("\nProcess\tMaximum Resource");
+	for(i=0;i<no_of_pro;i++)
+	{	
+		printf("\n%d\t",i);
+		for(j=0;j<no_of_res;j++)
+		{
+			printf("%c->%d\t",j+65,max[i][j]);		
+		}
+	}
+	printf("\nProcess\tNeed");
+	for(i=0;i<no_of_pro;i++)
+	{	
+		printf("\n%d\t",i);
+		for(j=0;j<no_of_res;j++)
+		{
+			printf("%c->%d\t",j+65,need[i][j]);		
+		}
+	}
+	sem_post(&a);	
+}
+void *check()
+{	
+	sem_wait(&a);
+	printf("\n");
+	int i,j,count;
+	for(i=0;i<no_of_pro;i++)
+	{
+		count=0;
+		if(finish[i]==0)
+		{
+			for(j=0;j<no_of_res;j++)
+			{
+				if(need[i][j]<=work[j])
+				count++;
+			}
+			if(count==no_of_res)
+			{
+				printf(" P%d \t",i);
+				finish[i]=1;
+				for(j=0;j<no_of_res;j++)
+				{	
+					work[j]=work[j]+alloc[i][j];
+				}
+				i=-1;
+			}
+		}
+	}
+	count=0;
+	for(i=0;i<no_of_pro;i++)
+	{
+		if(finish[i]==1)
+		{
+			count++;
+		}
+	}
+	if(count==no_of_pro)
+	{
+		printf("\nSafe State\n");
+	}
+	else
+	printf("\nUnsafe state\n");
+}
+int main()
+{	
+int i,j;
+no_of_pro=5;
+no_of_res=3;
+inst[0]=10;inst[1]=5;inst[2]=7;
+avail_inst[0]=3;avail_inst[1]=3;avail_inst[2]=2;
+alloc[0][0]=0;alloc[0][1]=1;alloc[0][2]=0;
+alloc[1][0]=2;alloc[1][1]=0;alloc[1][2]=0;
+alloc[2][0]=3;alloc[2][1]=0;alloc[2][2]=2;
+alloc[3][0]=2;alloc[3][1]=1;alloc[3][2]=1;
+alloc[4][0]=0;alloc[4][1]=0;alloc[4][2]=2;
+max[0][0]=7;max[0][1]=5;max[0][2]=3;
+max[1][0]=3;max[1][1]=2;max[1][2]=2;
+max[2][0]=9;max[2][1]=0;max[2][2]=2;
+max[3][0]=2;max[3][1]=2;max[3][2]=2;
+max[4][0]=4;max[4][1]=3;max[4][2]=3;
+for(i=0;i<no_of_pro;i++)
+	{	
+	finish[i]=0;
+	for(j=0;j<no_of_res;j++)
+		{
+		work[j]=avail_inst[j];
+		if(max[i][j]>inst[j])
+				{
+					printf("Process is asking for invalid number of resources. Error.\n");
+					exit(0);
+				}
+		need[i][j]=max[i][j]-alloc[i][j];
+		}
+	}
+	sem_init(&a,1,0);
+	pthread_t t1,t2;
+	pthread_create(&t1,NULL,&display,NULL);
+	pthread_create(&t2,NULL,&check,NULL);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+}
